ca1100ef15696e4479d1f088f13fd841ccce5a62
photos <myjellymail@gmail.com>
2016-05-30 18:39:26 -0700

    added searchbar, csv parsing, and collectionView

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Forrest Collins on 5/26/16.
+//  Copyright © 2016 bananapps. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
